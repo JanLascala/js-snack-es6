1bb90475e4a0f06bb8af4a93b5e45b6bd76f18ad
JanLascala <lascala.jan@gmail.com>
2025-02-21 16:36:51 +0100

    add no solution

---
+++ snack-2/main.js
@@ -0,0 +1,28 @@
+console.log('hi')
+
+const teams = [
+    {
+        name: 'name team 1',
+        scores: '0',
+        penalties: '0',
+
+    },
+    {
+        name: 'name team 2',
+        scores: '0',
+        penalties: '0',
+
+    },
+    {
+        name: 'name team 3',
+        scores: '0',
+        penalties: '0',
+
+    },
+]
+
+for (let key in teams) {
+    Math.random(teams.scores)
+}
+
+console.log(teams)
