03c0f4d841799c5d978129b97a4bd7ae5c8954d1
JanLascala <lascala.jan@gmail.com>
2025-02-21 15:38:13 +0100

    add bikes

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,1 +1,21 @@
 console.log('work')
+
+const bici = [
+    {
+        type: 'zephyr',
+        weight: '30'
+    }
+    {
+        type: 'tundra',
+        weight: '60'
+    }
+    {
+        type: 'honeybadger',
+        weight: '15'
+    }
+    {
+        type: 'fossil',
+        weight: '70'
+    }
+]
+
